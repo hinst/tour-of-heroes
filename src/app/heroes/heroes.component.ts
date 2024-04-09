4b1948958881822c15d82ed7e1ea0acd3c673e64
hinst <hinstance@yandex.ru>
2024-04-09 21:18:49 +0200

    Adding heroes component

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+    selector: 'app-heroes',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './heroes.component.html',
+    styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm',
+    };
+}
